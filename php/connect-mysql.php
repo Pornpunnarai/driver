0a4a036d688569f863faa84604f66a09b59b0d08
paruj <paruj_lertjiranuwat@cmu.ac.th>
2018-07-26 13:45:42 +0700

    fix driver database

--- php/connect-mysql.php
+++ php/connect-mysql.php
@@ -1,11 +1,22 @@
 <?php
-
 $serverName = "localhost";
 $username = "root";
 $password = "";
 $dbName = "driver";
-$char_set = "charset=utf8";
+
+
 $objCon = mysqli_connect($serverName,$username,$password,$dbName);
+
+if(!$objCon){
+    $serverName = "localhost";
+    $username = "root";
+    $password = "1q2w3e4r";
+    $dbName = "driver";
+    $objCon = mysqli_connect($serverName,$username,$password,$dbName);
+}
+
+
 mysqli_set_charset($objCon,"utf8");
+
 
 ?>
